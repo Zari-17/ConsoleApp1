4c1bac709dc4397758881e6ff3a941303bcb68d4
Sarmad Zaryan <kha_zaryan@yahoo.com>
2020-07-23 22:57:27 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hi! My name is "+ Name + " And my Age is " + Age);
+        }*/
+    }
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            Person person = new Person();
+            person.Name = "John";
+            person.Age = 28;
+            person.HasPet = true;
+
+            person.Greeting();
+
+
+            Console.ReadLine();*/
+        }
+    }
+}
